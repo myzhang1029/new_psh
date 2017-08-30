302240c945838fc501768a69ee4ef2c25c1690ec
袁冬梅 <myzhang1029@163.com>
2017-08-30 10:49:08 +0800

    Update cd

--- builtin_command.c
+++ builtin_command.c
@@ -24,36 +24,28 @@ int builtin_command(char *command, char **parameters)
     {
         char *cd_path = NULL;
 
-        if(parameters[1] == NULL)
-        //make "cd" to "cd ~" as in bash
-        {
-            parameters[1] = malloc(3 * sizeof(char));
-            parameters[1][0]= '~';
-            parameters[1][2]= '\0';
-        }
-        if(parameters[1][0] == '~')
+        if(parameters[1][0] == '~' || parameters[1] == NULL/* 'cd' */)
         {
             cd_path = malloc(strlen(pwd->pw_dir)+strlen(parameters[1]));
             //'~' makes length 1 more,but instead of '\0'
             if(cd_path == NULL)
             {
-                printf("cd: malloc failed.\n");
+                printf("cd: malloc failed, please retry.\n");
             }
             strcpy(cd_path,pwd->pw_dir);
             strncpy(cd_path+strlen(pwd->pw_dir),parameters[1]+1,strlen(parameters[1]));
-			//printf("path with ~:\n%s\n",cd_path);
         }
         else
         {
             cd_path = malloc(strlen(parameters[1]+1));
             if(cd_path == NULL)
             {
-                printf("cd: malloc failed.\n");
+                printf("cd: malloc failed, please retry.\n");
             }
             strcpy(cd_path,parameters[1]);
         }
         if(chdir(cd_path)!= 0)
-            printf("-psh: cd: %s:%s\n",cd_path,strerror(errno));
+            printf("cd: %s: %s\n", strerror(errno), cd_path);
         free(cd_path);
     }
     return 0;
