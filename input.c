060c4b06dcf264c3926d68c6864c4cfc6a58ab00
myzhang1029 <myzhang1029@163.com>
2017-12-31 23:19:59 +0800

    Enable no readline

--- input.c
+++ input.c
@@ -17,7 +17,12 @@ int read_command(char **command,char **parameters,char *prompt)
 {
 	char *buffer=malloc(sizeof(char)*MAXLINE);
 	memset(buffer, 0, sizeof(char)*MAXLINE);
+#ifndef NO_HISTORY
 	buffer = readline(prompt);
+#else
+	printf(prompt);
+	fgets(buffer, MAXLINE, stdin);
+#endif
 	if(feof(stdin))
 	{
 		printf("\n");
