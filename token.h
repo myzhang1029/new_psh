0851b98b72d819c27a5bf2e561ee76865eea7224
myzhang1029 <myzhang1029@163.com>
2018-08-30 10:38:33 +0800

    migrate token.h

--- token.h
+++ token.h
@@ -18,40 +18,40 @@
 
 enum psh_tokens
 {
-	IF,		     /* if */
-	THEN,		     /* then */
-	ELSE,		     /* else */
-	ELIF,		     /* elif */
-	FI,		     /* fi */
-	CASE,		     /* case */
-	ESAC,		     /* esac */
-	FOR,		     /* for */
-	SELECT,		     /* select */
-	WHILE,		     /* while */
-	UNTIL,		     /* until */
-	DO,		     /* do */
-	DONE,		     /* done */
-	FUNCTION,	    /* function */
-	COPROC,		     /* coproc */
-	IN,		     /* in */
-	BANG,		     /* ! */
-	TIME,		     /* time */
-	WORD, ASSIGNMENT,    /* = */
-	NUMBER, ARITH_CMD,   /*  */
-	ARITH_FOR_EXPRS,     /*  */
-	AND_AND,	     /* && */
-	OR_OR,		     /* || */
-	GREATER_GREATER,     /* >> */
-	LESS_LESS,	   /* << */
-	LESS_AND,	    /* <& */
-	LESS_LESS_LESS,      /* <<< */
-	GREATER_AND,	 /* >& */
-	SEMI_SEMI,	   /* ;; */
-	SEMI_AND,	    /* ;& */
-	SEMI_SEMI_AND,       /* ;;& */
-	LESS_LESS_MINUS,     /* <<- */
-	AND_GREATER,	 /* &> */
-	AND_GREATER_GREATER, /* &>> */
-	LESS_GREATER,	/* <& */
-	GREATER_BAR,	 /* >- */
-	BAR_AND,	     /* -& */
+        IF,                  /* if */
+        THEN,                /* then */
+        ELSE,                /* else */
+        ELIF,                /* elif */
+        FI,                  /* fi */
+        CASE,                /* case */
+        ESAC,                /* esac */
+        FOR,                 /* for */
+        SELECT,              /* select */
+        WHILE,               /* while */
+        UNTIL,               /* until */
+        DO,                  /* do */
+        DONE,                /* done */
+        FUNCTION,            /* function */
+        COPROC,              /* coproc */
+        IN,                  /* in */
+        BANG,                /* ! */
+        TIME,                /* time */
+        WORD, ASSIGNMENT,    /* = */
+        NUMBER, ARITH_CMD,   /*  */
+        ARITH_FOR_EXPRS,     /*  */
+        AND_AND,             /* && */
+        OR_OR,               /* || */
+        GREATER_GREATER,     /* >> */
+        LESS_LESS,           /* << */
+        LESS_AND,            /* <& */
+        LESS_LESS_LESS,      /* <<< */
+        GREATER_AND,         /* >& */
+        SEMI_SEMI,           /* ;; */
+        SEMI_AND,            /* ;& */
+        SEMI_SEMI_AND,       /* ;;& */
+        LESS_LESS_MINUS,     /* <<- */
+        AND_GREATER,         /* &> */
+        AND_GREATER_GREATER, /* &>> */
+        LESS_GREATER,        /* <& */
+        GREATER_BAR,         /* >- */
+        BAR_AND,             /* -& */
