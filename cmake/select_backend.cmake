b2d38772e5fced9d213e3163d6801cd5fcccd1f3
Zhang Maiyun <myzhang1029@hotmail.com>
2020-11-22 22:27:31 +0800

    Revert merge
    This is a control commit and takes no effect
    
    Signed-off-by: Zhang Maiyun <myzhang1029@hotmail.com>

--- cmake/select_backend.cmake
+++ cmake/select_backend.cmake
@@ -1,6 +1,6 @@
 # Select backend
 if (UNIX)
-	add_subdirectory(backends/posix)
+	add_subdirectory(backends/posix2)
 else()
 	message(FATAL_ERROR "Currently unsupported host system: " ${CMAKE_SYSTEM})
 endif()
