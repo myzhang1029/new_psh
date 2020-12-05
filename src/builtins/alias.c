40b461a4acede88733499466c8dcf20e95bc06ea
Manuel Bertele <manuel.bertele2003@gmail.com>
2020-12-05 02:22:23 +0100

    Added documentation
    Signed-off-by: Manuel Bertele <manuel.bertele2003@gmail.com>

--- src/builtins/alias.c
+++ src/builtins/alias.c
@@ -33,17 +33,23 @@ unsigned int amount_of_aliases = 0;
 
 void add_alias(psh_hash *table, char *alias, char *value)
 {
+    /* Add alias with is value to a hash table */
     psh_hash_add_chk(table, alias, value, 0);
 
+    /* If the list of aliases is enpty create one */
     if (list_of_aliases == NULL)
     {
         list_of_aliases = malloc(sizeof(char *));
     }else
     {
+        /* If it exists then reallocate it to fit another alias in */
         list_of_aliases = realloc(list_of_aliases, (amount_of_aliases + 1) * sizeof(char *));
     }
+    /* Set a pointer to a new alias and increase the amount of aliases */
     list_of_aliases[amount_of_aliases] = alias;
     amount_of_aliases++;
+
+    /* This list mainly exists for the alias command to be able to display all alises */
 
 }
 
@@ -132,7 +138,7 @@ int remove_alias(psh_state *state, char *alias)
         {
             if (list_of_aliases[j] != NULL)
             {
-
+                /* If alias to remove is in the list if all aliases */
                 if (!strcmp(alias, list_of_aliases[j]))
                 {
                     /* Remove alias from list_of_aliases */
@@ -156,7 +162,7 @@ int builtin_unalias(int argc, char **argv, psh_state *state)
     }
 
     int i;
-
+    /* Remove all aliases */
     if (!strcmp(argv[1], "-a"))
     {
 
@@ -165,15 +171,15 @@ int builtin_unalias(int argc, char **argv, psh_state *state)
             remove_alias(state, list_of_aliases[i]);
         }
        
-        return 0;
-    }
-
-
-    for (i = 1; i < argc; i++)
+    } else
     {
-        remove_alias(state, argv[i]);
+        /* Remove given aliases */
+
+        for (i = 1; i < argc; i++)
+        {
+            remove_alias(state, argv[i]);
+        }
     }
-   
     return 0;
 }
 
@@ -185,13 +191,15 @@ char *expand_alias(psh_hash *table, char *buffer)
     char *bufferv0;
     int i;
 
+    /* Check if the command contains a space */
     if (strstr(buffer, " "))
     {
-
+        /* Find the position of the space */
         for (i = 0; i < strlen(buffer); i++)
         {
             if(buffer[i] == ' ')
             {
+                /* Split the string into the command and its arguments */
                 buffer[i] = '\0';
                 after_argv0 = strdup(&buffer[i + 1]);
                 bufferv0 = strdup(buffer);
@@ -201,25 +209,30 @@ char *expand_alias(psh_hash *table, char *buffer)
         }
     } else
     {
+        /* If no space is in the command */
         bufferv0 = strdup(buffer);
     }
 
-
+    /* Find the value of the command */
     char *alias = check_for_alias(table, bufferv0);
 
+    /* If command is no alias then point to the original command */
     if (alias == NULL)
         alias = bufferv0;
 
+    /* Join the command and its arguments again */
     char *ret = xmalloc(strlen(alias) + strlen(after_argv0) + 2);
     sprintf(ret, "%s %s", alias, after_argv0);
 
+    /* Determine a space in the alias */
+    /* This is done to prepare for a possible upcomming recusion */
     for (i = 0; i < strlen(alias); i++)
     {
         if (alias[i] == ' ')
             alias[i] = '\0';
     }
 
-    /* Recusion if alias value is an alias */
+    /* Recusion if alias value is an alias itself */
     if (check_for_alias(table, alias) != NULL)
     {
         return expand_alias(table, ret);
