131e237c824afbf36d811effe3dd3bc625899b8f
Zhang Maiyun <myzhang1029@hotmail.com>
2020-12-05 09:23:21 +0800

    Finish expansion of $'' and $""
    Tested both.
    
    Signed-off-by: Zhang Maiyun <myzhang1029@hotmail.com>

--- src/expansion.c
+++ src/expansion.c
@@ -25,6 +25,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "libpsh/stringbuilder.h"
 #include "libpsh/util.h"
@@ -32,6 +33,261 @@
 #include "psh.h"
 #include "util.h"
 #include "variable.h"
+
+/* Tested */
+/** Expand $"", @p start_point pointing to the $. Quotes included in the
+ * returning value. */
+static char *expand_translation(psh_state *state, const char *start_point,
+                                const char **last_char)
+{
+    /* TODO: Gettext support */
+    char *cur, *result;
+    int n_backslash = 0;
+    cur = result = psh_strdup(start_point + 1); /* '"' */
+
+    while (*++cur)
+    {
+        if (*cur == '"')
+        {
+            if (cur[-1] == '\\')
+            {
+                ++n_backslash;
+                memmove(cur - 1, cur, strlen(cur) + 1);
+                --cur;
+            }
+            else
+            {
+                if (last_char)
+                    *last_char = cur - result + start_point;
+                return result;
+            }
+        }
+    }
+    psh_code_fault(state, __FILE__, __LINE__);
+}
+
+#if defined(DEBUG) && defined(TESTING_EXPAND_TRANSLATION)
+int main(void)
+{
+    psh_state state;
+    char *val;
+    state.argv0 = "test";
+    val = expand_translation(&state, "$\"tran\\\"slated\"", NULL);
+    puts(val); /* should yield "\"tran\"slated\"""" */
+    xfree(val);
+    return 0;
+}
+#endif
+
+/* Tested */
+/** Expand ANSI-C Quoting, @sa
+ * https://www.gnu.org/software/bash/manual/html_node/ANSI_002dC-Quoting.html
+ */
+static char *expand_ansi_quote(psh_state *state, const char *start_point,
+                               const char **last_char)
+{
+    /* The result is single-quoted, so the first quote comes from start_point */
+    const char *cur_char = ++start_point; /* points to the first '\'' now */
+    size_t cur_len = 0;
+    int last_char_is_backslash = 0;
+    psh_stringbuilder *builder = psh_stringbuilder_create();
+
+#define ANSI_QUOTE_COMMON_ADD(str)                                             \
+    do                                                                         \
+    {                                                                          \
+        psh_stringbuilder_add(builder, (str), 0);                              \
+        start_point = cur_char + 1;                                            \
+        cur_len = 0;                                                           \
+        last_char_is_backslash = 0;                                            \
+    } while (0)
+    while (1)
+    {
+        ++cur_len;  /* first-run: 1, for '\'' */
+        ++cur_char; /* First-run: points to the char after '\'' */
+        switch (*cur_char)
+        {
+            case 0:
+                /* This should not happen. Incomplete patterns should be
+                 * trapped by someone else */
+                psh_stringbuilder_free(builder);
+                psh_code_fault(state, __FILE__, __LINE__);
+            case '\\':
+                if (last_char_is_backslash)
+                {
+                    ANSI_QUOTE_COMMON_ADD("\\");
+                }
+                else
+                {
+                    psh_stringbuilder_add_length(builder, start_point, cur_len,
+                                                 0);
+                    last_char_is_backslash = 1;
+                    /* It is the escaped character's duty to set start_point
+                     */
+                }
+                break;
+            case 'a':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\a");
+                break;
+            case 'b':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\b");
+                break;
+            case 'e':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\033");
+                break;
+            case 'f':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\f");
+                break;
+            case 'n':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\n");
+                break;
+            case 'r':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\r");
+                break;
+            case 't':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\t");
+                break;
+            case 'v':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\v");
+                break;
+            case '"':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("\"");
+                break;
+            case '?':
+                if (last_char_is_backslash)
+                    ANSI_QUOTE_COMMON_ADD("?");
+                break;
+            case 'c':
+                if (last_char_is_backslash)
+                {
+                    char *c = xmalloc(1 * P_CS);
+                    *c = cur_char[1] - 'a' + 1;
+                    psh_stringbuilder_add_length(builder, c, 1, 1);
+                    ++cur_char;
+                    start_point = cur_char + 1;
+                    cur_len = 0;
+                    last_char_is_backslash = 0;
+                }
+                break;
+            case '\'':
+            {
+                char *result;
+                if (last_char_is_backslash)
+                {
+                    /* Even '"' cannot escape '\'', to test, try $'"'" in
+                     * bash */
+                    ANSI_QUOTE_COMMON_ADD("'");
+                    break;
+                }
+                /* This function is guaranteed to return given that the
+                 * pattern is complete */
+                psh_stringbuilder_add_length(
+                    builder, start_point, cur_len + 1 /* Include this '\'' */,
+                    0);
+                result = psh_stringbuilder_yield(builder);
+                psh_stringbuilder_free(builder);
+                if (last_char)
+                    *last_char = cur_char;
+                return result;
+            }
+            case '0':
+                if (last_char_is_backslash)
+                {
+                    char *num = xmalloc(4 * P_CS), *endptr;
+                    psh_strncpy(num, cur_char, 3);
+                    /* num is reused to avoid another allocation */
+                    num[0] = (char)strtol(num, &endptr, 8);
+                    psh_stringbuilder_add_length(builder, num, 1, 1);
+                    start_point = cur_char + (endptr - num);
+                    cur_char = start_point - 1;
+                    cur_len = 0;
+                    last_char_is_backslash = 0;
+                }
+                break;
+            case 'x':
+                if (last_char_is_backslash)
+                {
+                    char *num = xmalloc(3 * P_CS), *endptr;
+                    psh_strncpy(num, ++cur_char, 2);
+                    /* num is reused to avoid another allocation */
+                    num[0] = (char)strtol(num, &endptr, 16);
+                    psh_stringbuilder_add_length(builder, num, 1, 1);
+                    start_point = cur_char + (endptr - num);
+                    cur_char = start_point - 1;
+                    cur_len = 0;
+                    last_char_is_backslash = 0;
+                }
+                break;
+            case 'u':
+                /* XXX: UTF-16, unsupported. */
+                if (last_char_is_backslash)
+                {
+                    char *num = xmalloc(5 * P_CS), *endptr;
+                    psh_strncpy(num, ++cur_char, 4);
+                    /* num is reused to avoid another allocation */
+                    num[0] = (int)strtol(num, &endptr, 16);
+                    psh_stringbuilder_add_length(builder, num, 1, 1);
+                    start_point = cur_char + (endptr - num);
+                    cur_char = start_point - 1;
+                    cur_len = 0;
+                    last_char_is_backslash = 0;
+                }
+                break;
+            case 'U':
+                /* XXX: UTF-32, unsupported. */
+                if (last_char_is_backslash)
+                {
+                    char *num = xmalloc(9 * P_CS), *endptr;
+                    psh_strncpy(num, ++cur_char, 8);
+                    /* num is reused to avoid another allocation */
+                    num[0] = (int)strtol(num, &endptr, 16);
+                    psh_stringbuilder_add_length(builder, num, 1, 1);
+                    start_point = cur_char + (endptr - num);
+                    cur_char = start_point - 1;
+                    cur_len = 0;
+                    last_char_is_backslash = 0;
+                }
+                break;
+            default:;
+        }
+    }
+}
+
+#if defined(DEBUG) && defined(TESTING_EXPAND_ANSI_QUOTE)
+int main(void)
+{
+    psh_state state;
+    char *val;
+    state.argv0 = "test";
+    val = expand_ansi_quote(&state, "$'a\\na'", NULL);
+    puts(val); /* should yield "'a<newline>a'" */
+    xfree(val);
+    val = expand_ansi_quote(&state, "$'a\\012a'", NULL);
+    puts(val); /* should yield "'a<newline>a'" */
+    xfree(val);
+    val = expand_ansi_quote(&state, "$'a\\cja'", NULL);
+    puts(val); /* should yield "'a<newline>a'" */
+    xfree(val);
+    val = expand_ansi_quote(&state, "$'a\\x0aa'", NULL);
+    puts(val); /* should yield "'a<newline>a'" */
+    xfree(val);
+    val = expand_ansi_quote(&state, "$'a\\u000aa'", NULL);
+    puts(val); /* should yield "'a<newline>a'" */
+    xfree(val);
+    val = expand_ansi_quote(&state, "$'a\\U0000000aa'", NULL);
+    puts(val); /* should yield "'a<newline>a'" */
+    xfree(val);
+    return 0;
+}
+#endif
 
 /** Expand dollar signs. The caller is responsible of supplying a good pattern,
  * or an error will be emitted.
@@ -46,186 +302,7 @@ static char *expand_dollar(psh_state *state, const char *start_point,
                            const char **last_char)
 {
     if (start_point[1] == '\'')
-    {
-        /** ANSI-C Quoting, @sa
-         * https://www.gnu.org/software/bash/manual/html_node/ANSI_002dC-Quoting.html
-         */
-        const char *cur_char = start_point;
-        size_t cur_len = 0;
-        int last_char_is_backslash = 0;
-        psh_stringbuilder *builder = psh_stringbuilder_create();
-
-#define ANSI_QUOTE_COMMON_ADD(str)                                             \
-    do                                                                         \
-    {                                                                          \
-        psh_stringbuilder_add(builder, (str), 0);                              \
-        start_point = cur_char + 1;                                            \
-        cur_len = 0;                                                           \
-        last_char_is_backslash = 0;                                            \
-    } while (0)
-        /* The result is single-quoted */
-        psh_stringbuilder_add(builder, "'", 0);
-        while (1)
-        {
-            ++cur_len;
-            ++cur_char;
-            switch (*cur_char)
-            {
-                case 0:
-                    /* This should not happen. Incomplete patterns should be
-                     * trapped by someone else */
-                    psh_stringbuilder_free(builder);
-                    psh_code_fault(state, __FILE__, __LINE__);
-                case '\\':
-                    if (last_char_is_backslash)
-                    {
-                        ANSI_QUOTE_COMMON_ADD("\\");
-                    }
-                    else
-                    {
-                        psh_stringbuilder_add_length(
-                            builder, start_point, cur_len - 1 /* Remove '\\' */,
-                            0);
-                        last_char_is_backslash = 1;
-                        /* It is the escaped character's duty to set start_point
-                         */
-                    }
-                    break;
-                case 'a':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\a");
-                    break;
-                case 'b':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\b");
-                    break;
-                case 'e':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\033");
-                    break;
-                case 'f':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\f");
-                    break;
-                case 'n':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\n");
-                    break;
-                case 'r':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\r");
-                    break;
-                case 't':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\t");
-                    break;
-                case 'v':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\v");
-                    break;
-                case '"':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("\"");
-                    break;
-                case '?':
-                    if (last_char_is_backslash)
-                        ANSI_QUOTE_COMMON_ADD("?");
-                    break;
-                case 'c':
-                    if (last_char_is_backslash)
-                    {
-                        char *c = xmalloc(1 * P_CS);
-                        *c = cur_char[1] - 'a' + 1;
-                        psh_stringbuilder_add_length(builder, c, 1, 1);
-                        start_point = cur_char + 1;
-                        ++cur_char;
-                        cur_len = 0;
-                        last_char_is_backslash = 0;
-                    }
-                    break;
-                case '\'':
-                {
-                    char *result;
-                    if (last_char_is_backslash)
-                    {
-                        /* Even '"' cannot escape '\'', to test, try $'"'" in
-                         * bash */
-                        ANSI_QUOTE_COMMON_ADD("'");
-                        break;
-                    }
-                    /* This function is guaranteed to return given that the
-                     * pattern is complete */
-                    psh_stringbuilder_add_length(builder, start_point, cur_len,
-                                                 0);
-                    psh_stringbuilder_add(builder, "'", 0);
-                    result = psh_stringbuilder_yield(builder);
-                    psh_stringbuilder_free(builder);
-                    if (last_char)
-                        *last_char = cur_char;
-                    return result;
-                }
-                case '0':
-                    if (last_char_is_backslash)
-                    {
-                        char *num = xmalloc(4 * P_CS), *endptr;
-                        psh_strncpy(num, cur_char, 3);
-                        /* num is reused to avoid another allocation */
-                        num[0] = (char)strtol(num, &endptr, 8);
-                        psh_stringbuilder_add_length(builder, num, 1, 1);
-                        cur_char = endptr;
-                        start_point = cur_char + 1;
-                        cur_len = 0;
-                        last_char_is_backslash = 0;
-                    }
-                    break;
-                case 'x':
-                    if (last_char_is_backslash)
-                    {
-                        char *num = xmalloc(3 * P_CS), *endptr;
-                        psh_strncpy(num, cur_char + 1, 2);
-                        /* num is reused to avoid another allocation */
-                        num[0] = (char)strtol(num, &endptr, 16);
-                        psh_stringbuilder_add_length(builder, num, 1, 1);
-                        cur_char = endptr;
-                        start_point = cur_char + 1;
-                        cur_len = 0;
-                        last_char_is_backslash = 0;
-                    }
-                    break;
-                case 'u':
-                    /* XXX: UTF-16, unsupported. */
-                    if (last_char_is_backslash)
-                    {
-                        char *num = xmalloc(5 * P_CS), *endptr;
-                        psh_strncpy(num, cur_char + 1, 4);
-                        /* num is reused to avoid another allocation */
-                        num[0] = (int)strtol(num, &endptr, 16);
-                        psh_stringbuilder_add_length(builder, num, 1, 1);
-                        cur_char = endptr;
-                        start_point = cur_char + 1;
-                        cur_len = 0;
-                        last_char_is_backslash = 0;
-                    }
-                    break;
-                case 'U':
-                    /* XXX: UTF-32, unsupported. */
-                    if (last_char_is_backslash)
-                    {
-                        char *num = xmalloc(9 * P_CS), *endptr;
-                        psh_strncpy(num, cur_char + 1, 8);
-                        /* num is reused to avoid another allocation */
-                        num[0] = (int)strtol(num, &endptr, 16);
-                        psh_stringbuilder_add_length(builder, num, 1, 1);
-                        cur_char = endptr;
-                        start_point = cur_char + 1;
-                        cur_len = 0;
-                        last_char_is_backslash = 0;
-                    }
-                    break;
-                default:;
-            }
-        }
-    }
+        return expand_ansi_quote(state, start_point, last_char);
     if (start_point[1] == '{')
     {
         const char *base_name_start = (start_point += 2);
@@ -261,22 +338,7 @@ static char *expand_dollar(psh_state *state, const char *start_point,
         }
     }
     if (start_point[1] == '"')
-    {
-        /* TODO: Gettext support */
-        const char *cur = start_point + 1;
-        while (*++cur)
-        {
-            if (*cur == '"' && cur[-1] != '\\')
-            {
-                char *result = xmalloc((cur - start_point + 1) * P_CS);
-                psh_strncpy(result, start_point, cur - start_point);
-                if (last_char)
-                    *last_char = cur;
-                return result;
-            }
-        }
-        psh_code_fault(state, __FILE__, __LINE__);
-    }
+        return expand_translation(state, start_point, last_char);
 
     if (start_point[1] == '(')
         ; /* TODO */
